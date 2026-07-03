9707c783ab792825d6682f8089b3c8928fe88f69
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-07-03 18:39:53 +0530

    Time: 7 ms (53.96%), Space: 61.5 MB (79.39%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -8,6 +8,7 @@ class Solution {
             dp[i][0]=matrix[i][0];
             sum+=dp[i][0];
         }
+        //Matrix[0][0]=has already been traversed , we dont need to do it again otherwise sum will increase by 1 if mattrix[0][0]=1
         for(int j=1;j<m;j++){
             dp[0][j]=matrix[0][j];
             sum+=dp[0][j];
