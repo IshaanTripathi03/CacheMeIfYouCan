cc26ecd541742d0f51b1c443c177255b5d4b2545
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-07-03 15:47:56 +0530

    Time: 2254 ms (36.59%), Space: 43 MB (70.61%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -9,22 +9,22 @@ class Solution {
         }
         return true;
     }
-    int f(int i,int n,String s,int dp[]){
-        if(i==n){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int min=Integer.MAX_VALUE;
-        for(int j=i;j<n;j++){
-            if(isPalindrome(i,j,s)){
-                int cost=1+f(j+1,n,s,dp);
-                min=Math.min(min,cost);
-            }
-        }
-        return dp[i]=min;
-    }
+    // int f(int i,int n,String s,int dp[]){
+    //     if(i==n){
+    //         return 0;
+    //     }
+    //     if(dp[i]!=-1){
+    //         return dp[i];
+    //     }
+    //     int min=Integer.MAX_VALUE;
+    //     for(int j=i;j<n;j++){
+    //         if(isPalindrome(i,j,s)){
+    //             int cost=1+f(j+1,n,s,dp);
+    //             min=Math.min(min,cost);
+    //         }
+    //     }
+    //     return dp[i]=min;
+    // }
     public int minCut(String s) {
         int n=s.length();
         int dp[]=new int[n+1];
