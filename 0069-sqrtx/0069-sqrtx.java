85f7ed1ea4acfb34a96b66b635a147b08eea5c2f
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-07-19 18:56:54 +0530

    Time: 1 ms (97.55%), Space: 42.4 MB (79.19%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left=1,right=x;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if((long)mid*mid>x){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return right;
+    }
+}
