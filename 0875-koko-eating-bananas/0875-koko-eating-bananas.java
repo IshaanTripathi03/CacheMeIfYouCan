c96a5cbffb699c594274c79101d0f39307339485
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-07-20 19:21:44 +0530

    Time: 12 ms (52.11%), Space: 48.3 MB (22.36%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,26 @@
+class Solution {
+    long func(int[] piles,int k){
+        long sum=0;
+        for(int i:piles){
+            sum+=i%k==0?i/k:i/k+1;
+        }
+        return sum;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int max=Integer.MIN_VALUE;
+        for(int i:piles){
+            max=Math.max(max,i);
+        }
+        int low=1,high=max;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(func(piles,mid)<=h){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
