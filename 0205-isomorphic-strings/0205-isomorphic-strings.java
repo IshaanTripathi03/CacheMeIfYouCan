3d2526b9a3ba24cd8ecb223c4ba136e4f03ed1e3
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-07-26 12:56:58 +0530

    Time: 5 ms (98.39%), Space: 43.7 MB (87.06%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> st=new HashMap<>();
-        HashMap<Character,Character> ts=new HashMap<>();
         int n1=s.length();
         int n2=t.length();
-        if(n1!=n2){
-            return false;
-        }
-        for(int i=0;i<n1;i++){
-            char ch1=s.charAt(i);
-            char ch2=t.charAt(i);
-            if(st.containsKey(ch1) && st.get(ch1)!=ch2){
+        int smap[]=new int[256];
+        int tmap[]=new int[256];
+        for(int i=0;i<s.length();i++){
+            int ch1=s.charAt(i);
+            int ch2=t.charAt(i);
+            if(smap[ch1]!=tmap[ch2]){
                 return false;
             }
-            if(ts.containsKey(ch2) && ts.get(ch2)!=ch1){
-                return false;
-            }
-            st.put(ch1,ch2);
-            ts.put(ch2,ch1);
+            smap[ch1]=i+1;
+            tmap[ch2]=i+1;
         }
         return true;
     }
 }
+// HashMap<Character,Character> st=new HashMap<>();
+// HashMap<Character,Character> ts=new HashMap<>();
+// if(n1!=n2){
+//     return false;
+// }
+// for(int i=0;i<n1;i++){
+//     char ch1=s.charAt(i);
+//     char ch2=t.charAt(i);
+//     if(st.containsKey(ch1) && st.get(ch1)!=ch2){
+//         return false;
+//     }
+//     if(ts.containsKey(ch2) && ts.get(ch2)!=ch1){
+//         return false;
+//     }
+//     st.put(ch1,ch2);
+//     ts.put(ch2,ch1);
+// }
+// return true;
