1acc10ed9616a8c717dbd63151b9d2ef037280f0
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-08-17 15:48:29 +0530

    Time: 324 ms (20.91%), Space: 47.7 MB (49.28%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,29 +1,4 @@
 class Solution {
-    // int f(int[] stoneValue,int i,int j,int[] prefix,int[][] dp){
-    //     if(i==j){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=-1){
-    //         return dp[i][j];
-    //     }
-    //     int max=Integer.MIN_VALUE;
-    //     for(int k=i;k<j;k++){
-    //         int leftSum=prefix[k]- (i>0?prefix[i-1]:0);
-    //         int rightSum=prefix[j]-prefix[k];
-    //         int points;
-    //         if(leftSum<rightSum){
-    //             points=leftSum+f(stoneValue,i,k,prefix,dp);
-    //         }
-    //         else if(rightSum<leftSum){
-    //             points=rightSum+f(stoneValue,k+1,j,prefix,dp);
-    //         }
-    //         else{
-    //             points=leftSum+Math.max(f(stoneValue,i,k,prefix,dp),f(stoneValue,k+1,j,prefix,dp));
-    //         }
-    //         max=Math.max(max,points);
-    //     }
-    //     return dp[i][j]=max;
-    // }
     public int stoneGameV(int[] stoneValue) {
         int n=stoneValue.length;
         int prefix[]=new int[n];
@@ -56,3 +31,28 @@ class Solution {
         return dp[0][n-1];
     }
 }
+// int f(int[] stoneValue,int i,int j,int[] prefix,int[][] dp){
+//     if(i==j){
+//         return 0;
+//     }
+//     if(dp[i][j]!=-1){
+//         return dp[i][j];
+//     }
+//     int max=Integer.MIN_VALUE;
+//     for(int k=i;k<j;k++){
+//         int leftSum=prefix[k]- (i>0?prefix[i-1]:0);
+//         int rightSum=prefix[j]-prefix[k];
+//         int points;
+//         if(leftSum<rightSum){
+//             points=leftSum+f(stoneValue,i,k,prefix,dp);
+//         }
+//         else if(rightSum<leftSum){
+//             points=rightSum+f(stoneValue,k+1,j,prefix,dp);
+//         }
+//         else{
+//             points=leftSum+Math.max(f(stoneValue,i,k,prefix,dp),f(stoneValue,k+1,j,prefix,dp));
+//         }
+//         max=Math.max(max,points);
+//     }
+//     return dp[i][j]=max;
+// }
