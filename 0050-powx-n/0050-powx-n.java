298ad41c6eeb5bca0ed819981c66cc4a106def91
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-08-28 11:31:28 +0530

    Time: 0 ms (100%), Space: 47.6 MB (85.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,17 +1,17 @@
 class Solution {
-    double f(double x,int n){
-        if(n==0){
-            return 1.0;
-        }
-        double half=f(x,n/2);
-        if(n%2==0){
-            return half*half;
-        }
-        if(n>0){
-            return half*half*x;
-        }
-        return  half*half/x;
-    }
+    // double f(double x,int n){
+    //     if(n==0){
+    //         return 1.0;
+    //     }
+    //     double half=f(x,n/2);
+    //     if(n%2==0){
+    //         return half*half;
+    //     }
+    //     if(n>0){
+    //         return half*half*x;
+    //     }
+    //     return  half*half/x;
+    // }
     public double myPow(double x, int n) {
         double ans=1.0;
         while(n!=0){
