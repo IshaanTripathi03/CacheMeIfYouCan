682ab6514de2463e675c25b39b6758de66edbc7c
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-09-10 18:10:36 +0530

    Time: 54 ms (65.11%), Space: 70.5 MB (55.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -4,14 +4,10 @@ class Solution {
         int left=0;
         int n=fruits.length,max=0;
         for(int right=0;right<n;right++){
-            
             map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
-            
             while(map.size()>2){
-                if(map.get(fruits[left])>1){
-                    map.put(fruits[left],map.get(fruits[left])-1);
-                }
-                else{
+                map.put(fruits[left],map.get(fruits[left])-1);
+                if(map.get(fruits[left])==0){
                     map.remove(fruits[left]);
                 }
                 left++;
