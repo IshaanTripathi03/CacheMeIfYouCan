983f0b6b99748f0c597bb2fb8e6705103607504f
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-09-14 18:32:22 +0530

    Time: 115 ms (12.41%), Space: 48 MB (13.94%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int n=s.length(), m=t.length();
+        int left=0;
+        int minlen=Integer.MAX_VALUE,count=0;
+        String result="";
+        for(int right=0;right<n;right++){
+            char ch=s.charAt(right);
+            if(map.containsKey(ch) && map.get(ch)>0){
+                count++;
+            }
+            map.put(ch,map.getOrDefault(ch,0)-1);
+            while(count==m){
+                char lftch=s.charAt(left);
+                if(minlen>right-left+1){
+                    minlen=right-left+1;
+                    result=s.substring(left,right+1);
+                }
+                map.put(lftch,map.get(lftch)+1);
+                if(map.get(lftch)>0){
+                    count--;
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
