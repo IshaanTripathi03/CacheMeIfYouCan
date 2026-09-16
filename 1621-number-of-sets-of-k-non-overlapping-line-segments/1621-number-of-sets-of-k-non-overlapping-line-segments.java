803bc3a95e2651c159a5ced4c1b8a1d208e8d280
Ishaan Tripathi <ishaantri0330@gmail.com>
2026-09-16 14:18:38 +0530

    Time: 20 ms (81.94%), Space: 88.1 MB (57.42%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,30 +1,17 @@
 class Solution {
     static final int MOD=1_000_000_007;
-    static int f(int n,int k,int dp[][]){
-        if(k==0){
-            return 1;
-        }
-        if(n==0){
-            return 0;
-        }
-        if(dp[n][k]!=-1){
-            return dp[n][k];
-        }
-        if(n-1==k){
-            return 1;
-        }
-        long sum=f(n-1,k,dp);
-        for(int start = 0; start < n - 1; start++) {
-            sum += f(start + 1, k - 1,dp);
-            sum%=MOD;
-        }
-        return dp[n][k]=(int)sum;
-    }
     public int numberOfSets(int n, int k) {
-        int dp[][]=new int[n+1][k+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int N=n+k-1;
+        int R=2*k;
+        int[][] dp=new int[N+1][R+1];
+        for(int i=0;i<=N;i++){
+            dp[i][0]=1;
         }
-        return f(n,k,dp);
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=R && j<=i;j++){
+                dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%MOD;
+            }
+        }
+        return dp[N][R];
     }
 }
